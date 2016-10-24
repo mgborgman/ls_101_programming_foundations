31184b3821fb95a62876dd42a61377d416fb88a4
Matt Borgman <mgborgman@gmail.com>
2016-10-23 17:08:50 -0700

    easy exercises - lesson 3

---
+++ lesson_3/exercise_6.rb
@@ -0,0 +1,11 @@
+famous_words = "seven years ago..."
+
+four_score = "Four score and "
+
+gettysburg_address = four_score + famous_words
+
+puts gettysburg_address
+
+gettysburg_address = four_score << famous_words
+
+puts gettysburg_address
---
+++ lesson_3/exercise_9.rb
@@ -0,0 +1,6 @@
+flinstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+flinstones_array = []
+flinstones_array[0] = flinstones.key(2)
+flinstones_array[1] = flinstones["Barney"]
+
+p(flinstones_array)
